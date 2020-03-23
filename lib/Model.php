bf9f6bb6449c48f7aee762aed8d4e19d3ae1eb08
Kenneth P. Hough <kenneth@egtech.us>
2020-03-23 12:12:38 +0900

    Create model and modelobject classes

---
+++ lib/Model.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Kyte;
+
+/*
+ * Class Model
+ *
+ * @package Kyte
+ *
+ */
+
+class Model
+{
+	// key-value describing model
+	// 
+	//	[
+	// 		'name'		=> 'name of table (also name of object)',
+	// 		'struct'	=> [
+	//			'column name' => [ 'type' => 'i/s/d', 'requred' => true/false ],
+	//			...
+	//			'column name' => [ 'type' => 'i/s/d', 'requred' => true/false ],
+	//		]
+	//	]
+	protected $model = [];
+
+	public $objects = [];
+
+	public function __construct($model) {
+		$this->model = $model;
+	}
+
+	public function retrieve($field = null, $value = null, $isLike = false, $conditions = null, $all = false)
+	{
+		try {
+			$dataObjects = array();
+			$data = array();
+
+			if (isset($field, $value)) {
+				if ($isLike) {
+					$sql = "WHERE `$field` LIKE '$value'";
+				} else {
+					$sql = "WHERE `$field` = '$value'";
+				}
+
+				if (!$all) {
+					$sql .= " AND `deleted` = '0'";
+				}
+
+				if(isset($conditions)) {
+					foreach($conditions as $condition) {
+						$sql .= " AND `{$condition['field']}` = '{$condition['value']}'";
+					}
+				}
+				$data = DBI::select($this->model['name'], null, $sql);
+			} else {
+				$data = $all ? DBI::select($this->model['name'], null, null) : DBI::select($this->model['name'], null, "WHERE `deleted` = '0'");
+			}
+
+			foreach ($data as $item) {
+				$obj = new \Kyte\ModelObject($this->model);
+				$obj->retrieve('id', $item['id'], null, null, $all);
+				$dataObjects[] = $obj;
+			}
+
+			$this->objects = $dataObjects;
+
+			return true;
+		} catch (\Exception $e) {
+			throw $e;
+			return false;
+		}
+	}
+
+	/*
+	 * Returns array count of objects in Model
+	 *
+	 */
+	public function count()
+	{
+		return count($this->objects);
+	}
+
+	protected function clearModel()
+	{
+		$this->objects = [];
+	}
+}
+?>
